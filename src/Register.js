4cb818476228dafc6e95014ca79c9272ed043516
SamMsdRoy <samvicky650@gmail.com>
2024-01-23 19:50:55 +0530

    t

---
+++ src/Register.js
@@ -0,0 +1,13 @@
+export default function Register(){
+    return (
+        <>
+            <form>
+                <div className="input-group">
+                <label className="label">Name</label>
+                <input className="form-control"/>
+                </div>
+                
+            </form>
+        </>
+    )
+}
