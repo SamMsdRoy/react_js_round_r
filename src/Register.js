b25f6d8d530e5c23b699d328df25124656bfd323
SamMsdRoy <samvicky650@gmail.com>
2024-01-23 20:17:30 +0530

    react first

--- src/Register.js
+++ src/Register.js
@@ -1,13 +1,32 @@
+import React from "react"
 export default function Register(){
     return (
         <>
+        <div className="card col-lg-8 text-center border-box">
             <form>
-                <div className="input-group">
-                <label className="label">Name</label>
-                <input className="form-control"/>
+                <div className="input-group m-3">
+                <label className="form-label">Name</label>
+                <input type="text" className="form-control"/>
                 </div>
-                
+                <div className="input-group">
+                <label className="label">Email</label>
+                <input type="email" className="form-control"/>
+                </div>
+                <div className="input-group">
+                <label className="label">Password</label>
+                <input type="password" className="form-control"/>
+                </div>
+                <div className="input-group">
+                <label className="label">Re-Password</label>
+                <input type="text" className="form-control"/>
+                </div>
+                <div className="btn-group">
+                    <input type="checkbox"/>
+                    <button type="button" >Submit</button>
+
+                </div>
             </form>
+        </div>
         </>
     )
 }
