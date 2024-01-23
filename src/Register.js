f0c924b2a5a0626fee60a778a9e902d62b12fc11
SamMsdRoy <samvicky650@gmail.com>
2024-01-23 21:06:44 +0530

    2nd commit

--- src/Register.js
+++ src/Register.js
@@ -1,32 +1,17 @@
 import React from "react"
 export default function Register(){
-    return (
-        <>
-        <div className="card col-lg-8 text-center border-box">
-            <form>
-                <div className="input-group m-3">
-                <label className="form-label">Name</label>
-                <input type="text" className="form-control"/>
-                </div>
-                <div className="input-group">
-                <label className="label">Email</label>
-                <input type="email" className="form-control"/>
-                </div>
-                <div className="input-group">
-                <label className="label">Password</label>
-                <input type="password" className="form-control"/>
-                </div>
-                <div className="input-group">
-                <label className="label">Re-Password</label>
-                <input type="text" className="form-control"/>
-                </div>
-                <div className="btn-group">
-                    <input type="checkbox"/>
-                    <button type="button" >Submit</button>
 
-                </div>
-            </form>
+    const [mail,setMail]=useState(0)
+    return (
+        <div className="card col-lg-8 col-md-6 col-sm-10 mr-auto">
+            <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" value={} class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>
+            <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
         </div>
-        </>
     )
 }
